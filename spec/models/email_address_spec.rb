da3bfcadea0476181b21b332724541ec1804187f
Sebastian Abondano <sabondano1@gmail.com>
2015-07-26 13:04:33 -0600

    Added validation to email address model as well as tests for it

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:email_address) { EmailAddress.new(address: 'bob@example.com') }
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without an address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
 end
